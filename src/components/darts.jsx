57eaca365279a7c674f9489caa09f62c3fd7100b
Luis <lerhardt@rhrk.uni-kl.de>
2023-02-24 02:35:25 +0100

    add credits

--- src/components/darts.jsx
+++ src/components/darts.jsx
@@ -37,6 +37,14 @@ class Darts extends Component {
         ) : (
           <Initialization startGame={this.startGame} />
         )}
+        <div id="referenceBox" className="position-absolute text-muted d-flex justify-content-center">
+          <div>
+            © 2023{" "}
+            <a className="text-muted" href="https://github.com/LuisErhardt" target="_blank" rel="noopener noreferrer">
+              Luis Erhardt
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
